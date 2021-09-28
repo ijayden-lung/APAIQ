553191d6332f164d4cc63a12989daf2ebdbeae09
ijayden-lung <ijayden.lung@gmail.com>
2021-09-29 01:43:06 +0300

    modified requirements

--- requirements.sh
+++ requirements.sh
@@ -7,6 +7,7 @@
 conda create -n 'APAIQ' python=3.7.3 biopython -y 
 conda activate 'APAIQ'
 #tensorflow=2.1.0
-conda install -c conda-forge tensorflow=2.1.0 -y
+#tensorflow-estimator=2.1.0
+conda install -c conda-forge tensorflow=2.1.0  tensorflow-estimator=2.1.0  -y
 #pandas=0.25.3
 conda install -c anaconda pandas=0.25.3  -y
